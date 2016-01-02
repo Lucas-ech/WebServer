ba7a5611eeec825c1f883f981f9f8253283ba242
lucas <lucas@echd.fr>
2016-01-02 18:53:58 +0100

    Add route with wildcard support

--- Router.cpp
+++ Router.cpp
@@ -15,6 +15,17 @@ void Router::connect(std::string path, std::function<void(std::map<std::string, 
 bool Router::route(std::string path) {
 	auto connection = m_connections.find(path);
 	if(connection == m_connections.end()) {
+		std::size_t wildcardPos;
+		for(auto&& route : m_connections) {
+			wildcardPos = route.first.find("*");
+			if(wildcardPos != std::string::npos) {
+				std::string base = route.first.substr(0, wildcardPos);
+				if(route.first.substr(0, wildcardPos) == path.substr(0, wildcardPos)) {
+					route.second({});
+					return true;
+				}
+			}
+		}
 		return false;
 	}
 
